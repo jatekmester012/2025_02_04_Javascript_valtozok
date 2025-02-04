19d3c42f95b30d099c7da9076b3bc61bd26eb1f2
ForgacsAlex012 <forgacs.alex@diak.szbi-pg.hu>
2025-02-04 09:38:47 +0100

    add .

--- 01_valtozok.js
+++ 01_valtozok.js
@@ -1,1 +1,19 @@
 console.log("Hello word!")
+
+var a = 2;
+console.log(a); 
+
+/*let b = 3;
+b = 4;
+const c = 5;
+c = 6; //hiba */
+
+var x = 5;
+console.log(x);
+var x = "alma";
+console.log(x);
+
+let d = 5;
+console.log(d);
+// let d = 7;
+// console.log(d);
---
+++ 02_valtozok.js
@@ -0,0 +1,9 @@
+let e = 2;
+console.log('a = ' + e + ' tipusa: ' + typeof e); 
+const pi =3.14;
+console.log('pi = ' + pi + ' tipusa: ' + typeof pi);
+
+
+
+console.log(0.1 + 0.2);
+console.log((0.1 + 0.2).toFixed(2));
