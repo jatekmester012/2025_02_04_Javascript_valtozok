0f9b7bc6a1d8dd474bfea030fda4b2deefc63c57
ForgacsAlex012 <forgacs.alex@diak.szbi-pg.hu>
2025-02-04 10:28:30 +0100

    add ..

--- 02_valtozok.js
+++ 02_valtozok.js
@@ -7,3 +7,30 @@ console.log('pi = ' + pi + ' tipusa: ' + typeof pi);
 
 console.log(0.1 + 0.2);
 console.log((0.1 + 0.2).toFixed(2));
+
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+console.log(`Hello, ${firstName} ${lastName}!`);
+
+let person = {
+    name: 'Gyenei Péter Kont',
+    age: 39,
+    isStudent: false
+}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.address);
+
+let xy;
+console.log(xy);
+
+console.log('BOOLEAN');
+console.log(typeof true);
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean("a"));
+console.log(Boolean(1));
